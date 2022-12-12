6aa982fb7285c08f760e70b3ed8a8deb3ac723d2
naffiziqbal <f.nafiziqbal@gmail.com>
2022-12-12 19:54:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
+import Typewriter from 'typewriter-effect';
+import topu from './66577357_777162262712551_3444778854764249088_n.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header ">
+        <div className="topuimg">
+          <img src={topu} alt="" className='topu' />
+        </div>
+        <Typewriter
+          options={{
+            strings: [
+      ` নাম তপু। ফেভারিট ডায়লগ ঃ - 'চৌধুরী সাহেব আমরা গরিব হতে পারি, কিন্তু আমরা মানুষ, আমাদেরও ইজ্জত আছে' `       
+
+        ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
       </header>
     </div>
   );
